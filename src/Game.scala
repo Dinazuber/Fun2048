3abbf4f8c404e936cffd22a2d44367baa75e7977
Dina <119934536+Dinazuber@users.noreply.github.com>
2025-12-18 16:15:22 +0100

    Adding height and the window

--- src/Game.scala
+++ src/Game.scala
@@ -3,5 +3,9 @@ import hevs.graphics.FunGraphics
 class Game {
   val screenWidth : Int = 1080
   var display : FunGraphics = new FunGraphics(screenWidth, screenWidth)
+  val height : Int = 4
+  val widthGameplay : Int = 960
+
+  val widthCell : Int = widthGameplay / height
 
 }
